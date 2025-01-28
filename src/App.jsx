3e59f600b4c1216d1b271a8f0c737e316a5784b8
Mattazz <matthewazada101@gmail.com>
2025-01-28 09:16:49 -0500

    Add Home component and update routing; enhance layout with flexbox styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,19 @@ import CustomFooter from './components/CustomFooter';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeroPage from './components/HeroPage';
-import { Container } from 'react-bootstrap';
+import Home from './components/Home';
 
 function App() {
   return (
     <Router>
       <div id="root">
         <CustomNav />
-        <div className="main-content"> {/* Main content inside this  */}
-          {/* Routing paths using react-router-dom */}
+        <div className={`main-content ${location.pathname === '/' ? 'full-height-container' : ''}`}>
+        {/* Routing paths using react-router-dom */}
           <Routes>
-            <Route path="/hero" element={<HeroPage />}/>
+            <Route path="/hero" element={<HeroPage />} />
+            <Route path="/" element={<Home />} />
           </Routes>
-
-          <Container className='border-danger border border-1 text-center'>
-          <h1>Welcome to my site.</h1>
-          <p>Click on the Hero link in the nav bar to continue.</p>
-          </Container>
-
         </div>
         <CustomFooter />
       </div>
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import { Container } from "react-bootstrap"
+
+function Home() {
+    return (
+        <>
+            <Container className='full-height-container'>
+                <div>
+                    <h1>Welcome to my site.</h1>
+                    <p>Click on the Hero link in the nav bar to continue.</p>
+                </div>
+            </Container>
+
+        </>
+    )
+}
+
+export default Home
